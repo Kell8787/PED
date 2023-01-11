038962184b63ca48292faa09e1bcf60cf016a10e
Rodrigo <00083121@uca.edu.sv>
2023-01-10 20:25:32 -0600

    :memo: Tarea Finalizada

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -30,13 +30,12 @@ void pedirDatos()
     a la estructura*/
     cout << "Ingrese su edad: ";
     cin >> persona1.edad; // Como la edad es de tipo entero, facilmente podemos guardarlo con un "cin", la variable persona1, nos permite ingresar a la estructura
-    cout << "Ingrese su deporte favorito: ";
-    cin.getline(persona1.deporte, 30, '\n');
+    
 }
 
 void mostrarDatos(DatosPersonales X)
 { // DatosPersonales sera siempre la estructura y tendra una variable x que accedera a los campos de la estructura (Llama X por conveniencia para no externder la funcion)
     cout << "\n\nNombre: " << X.nombre << endl;
     cout << "Edad: " << X.edad << endl;
-    cout << "Deporte: " << X.deporte << endl;
+    
 }
